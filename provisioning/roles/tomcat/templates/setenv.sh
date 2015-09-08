6a92d6a5b1ac2cf2a1bb1ec45aa99bf876d182d4
coderliux <ecliujie@gmail.com>
2015-09-08 18:56:01 +0800

    [JL] -#000 -change the script for tomcat

--- provisioning/roles/tomcat/templates/setenv.sh
+++ provisioning/roles/tomcat/templates/setenv.sh
@@ -1,3 +1,3 @@
 export JAVA_OPTS="-DdefaultLocale=en -DenvironmentName=local -Xmx1500m -XX:MaxPermSize=512m -Xms512m"
 #export CATALINA_OUT=/app/tomcat/openlmis/logs
-export CLASSPATH=$CLASSPATH:{{application_dir}}/{{application_name}}/properties/{{region}}
+export CLASSPATH=$CLASSPATH:{{application_dir}}/{{application_name}}/properties
