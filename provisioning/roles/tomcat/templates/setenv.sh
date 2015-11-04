4ff7a0ceba41a8e80f8f6b9876a2ed25e184c00a
coderliux <ecliujie@gmail.com>
2015-11-04 11:25:18 +0800

    [JL] -#000 -add heap dump option for tomcat

--- provisioning/roles/tomcat/templates/setenv.sh
+++ provisioning/roles/tomcat/templates/setenv.sh
@@ -1,3 +1,4 @@
-export JAVA_OPTS="-DdefaultLocale=en -DlogHome={{application_dir}}/{{application_name}}/logs -Xmx1500m -XX:MaxPermSize=512m -Xms512m"
+export JAVA_OPTS="-DdefaultLocale=en -DlogHome={{application_dir}}/{{application_name}}/logs -Xmx1500m -XX:MaxPermSize=512m -Xms512m  -XX:+HeapDumpOnOutOfMemoryError
+-XX:HeapDumpPath={{application_dir}}/{{application_name}}/logs/threaddump.hprof"
 #export CATALINA_OUT=/app/tomcat/openlmis/logs
 export CLASSPATH=$CLASSPATH:{{application_dir}}/{{application_name}}/properties
