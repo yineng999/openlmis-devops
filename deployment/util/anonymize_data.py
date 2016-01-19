1ade51dc52080489e9cbee88e0f41e875e5c9ba8
Danni Yu <dyu@thoughtworks.com>
2016-01-19 14:31:34 +0800

    add step for syncding data for prod sim environment

---
+++ deployment/util/anonymize_data.py
@@ -0,0 +1,17 @@
+from tempfile import mkstemp
+from shutil import move
+from os import remove, close
+import re
+import random
+
+def replace_email(file_path):
+    fh, abs_path = mkstemp()
+    with open(abs_path,'w') as new_file:
+        with open(file_path) as old_file:
+            for line in old_file:
+                new_file.write(re.sub("^(.*? |)[^@]+@[^ ]+", "x%s@xxx.com" % random.randint(0, 10000), line))
+    close(fh)
+    remove(file_path)
+    move(abs_path, file_path)
+
+replace_email("dumpForProdSim.sql")
