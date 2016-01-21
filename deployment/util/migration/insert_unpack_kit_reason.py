b2c4bff9534215ff633e9112614e55b0a7ddea4f
Yingli Yuan <ylyuan@thoughtworks.com>
2016-01-21 11:57:57 +0800

    MG&YY - Add db-migration role to CI env

---
+++ deployment/util/migration/insert_unpack_kit_reason.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import psycopg2
+import sys
+import pprint
+
+def main():
+    conn_string = "host='localhost' dbname='open_lmis' user='postgres' password=''"
+
+    print "Connecting to database\n ->%s" % (conn_string)
+
+    conn = psycopg2.connect(conn_string)
+
+    cursor = conn.cursor()
+    print "Connected!\n"
+
+    cursor.execute("SELECT * FROM losses_adjustments_types WHERE name = 'UNPACK_KIT'")
+
+    if(len(cursor.fetchall()) == 0) :
+        cursor.execute("INSERT INTO losses_adjustments_types (name, description, additive) VALUES('UNPACK_KIT', 'Unpack kit', FALSE)")
+
+    conn.commit()
+
+if __name__ == "__main__":
+    main()
