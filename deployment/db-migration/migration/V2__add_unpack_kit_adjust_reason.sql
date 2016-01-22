91644a7fb77ec370f02c859c2f20e0467a16417e
Yingli Yuan <ylyuan@thoughtworks.com>
2016-01-22 12:10:42 +0800

    YL - #000 - use insert if not exist not insert in db-migration

--- deployment/db-migration/migration/V2__add_unpack_kit_adjust_reason.sql
+++ deployment/db-migration/migration/V2__add_unpack_kit_adjust_reason.sql
@@ -1,1 +1,6 @@
-INSERT INTO losses_adjustments_types (name, description, additive) VALUES ('UNPACK_KIT', 'Unpack kit', FALSE);
+INSERT INTO losses_adjustments_types (name, description, additive)
+SELECT 'UNPACK_KIT', 'Unpack kit', FALSE
+WHERE
+    NOT EXISTS (
+        SELECT * FROM losses_adjustments_types WHERE name = 'UNPACK_KIT'
+    );
