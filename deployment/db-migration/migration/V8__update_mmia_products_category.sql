325ed1f26219a8858707686d8896139bc14b1629
Yingli Yuan <ylyuan@thoughtworks.com>
2016-04-18 15:46:25 +0800

    YL&MG- #592 - update category for mmia products.

---
+++ deployment/db-migration/migration/V8__update_mmia_products_category.sql
@@ -0,0 +1,96 @@
+DELETE FROM product_categories WHERE name NOT IN ('Adult', 'Children', 'Solution', 'Other');
+
+
+INSERT INTO product_categories (code, name, displayorder)
+SELECT '12', 'Adult', '12'
+WHERE
+    NOT EXISTS (
+        SELECT code FROM product_categories WHERE name = 'Adult'
+    );
+
+INSERT INTO product_categories (code, name, displayorder)
+SELECT '13', 'Children', '13'
+WHERE
+    NOT EXISTS (
+        SELECT code FROM product_categories WHERE name = 'Children'
+    );
+
+INSERT INTO product_categories (code, name, displayorder)
+SELECT '14', 'Solution', '14'
+WHERE
+    NOT EXISTS (
+        SELECT code FROM product_categories WHERE name = 'Solution'
+    );
+
+
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult')
+FROM products p WHERE p.id = program_products.productid AND p.code='08S42';
+
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult')
+FROM products p WHERE p.id = program_products.productid AND p.code='08S18';
+
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult')
+FROM products p WHERE p.id = program_products.productid AND p.code='08S40';
+
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult')
+FROM products p WHERE p.id = program_products.productid AND p.code='08S36';
+
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult')
+FROM products p WHERE p.id = program_products.productid AND p.code='08S32';
+
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult')
+FROM products p WHERE p.id = program_products.productid AND p.code='08S18';
+
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult')
+FROM products p WHERE p.id = program_products.productid AND p.code='08S39';
+
+  UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult')
+  FROM products p WHERE p.id = program_products.productid AND p.code='08S21';
+
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult')
+FROM products p WHERE p.id = program_products.productid AND p.code='08S01';
+
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult')
+FROM products p WHERE p.id = program_products.productid AND p.code='08S22';
+
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult')
+FROM products p WHERE p.id = program_products.productid AND p.code='08S13';
+
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult')
+FROM products p WHERE p.id = program_products.productid AND p.code='08S15';
+
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children')
+FROM products p WHERE p.id = program_products.productid AND p.code='08S34B';
+
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children')
+FROM products p WHERE p.id = program_products.productid AND p.code='08S32Z';
+
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children')
+FROM products p WHERE p.id = program_products.productid AND p.code='08S42B';
+
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children')
+FROM products p WHERE p.id = program_products.productid AND p.code='08S40Z';
+
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children')
+FROM products p WHERE p.id = program_products.productid AND p.code='08S39B';
+
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children')
+FROM products p WHERE p.id = program_products.productid AND p.code='08S39Y';
+
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children')
+FROM products p WHERE p.id = program_products.productid AND p.code='08S01ZZ';
+
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children')
+FROM products p WHERE p.id = program_products.productid AND p.code='08S20';
+
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children')
+FROM products p WHERE p.id = program_products.productid AND p.code='08S19';
+
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children')
+FROM products p WHERE p.id = program_products.productid AND p.code='08S01B';
+
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Solution')
+FROM products p WHERE p.id = program_products.productid AND p.code='08S23';
+
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Solution')
+FROM products p WHERE p.id = program_products.productid AND p.code='08S17';
