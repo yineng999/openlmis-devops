b7c3fa6808c13df811a1f798fb5b207052d795c3
Yingli Yuan <ylyuan@thoughtworks.com>
2016-04-19 16:10:49 +0800

    YL&MG- #592 - update modifieddate for mmia products.

--- deployment/db-migration/migration/V8__update_mmia_products_category.sql
+++ deployment/db-migration/migration/V8__update_mmia_products_category.sql
@@ -23,74 +23,74 @@ WHERE
     );
 
 
-UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult')
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult'), modifieddate = now()
 FROM products p WHERE p.id = program_products.productid AND p.code='08S42';
 
-UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult')
-FROM products p WHERE p.id = program_products.productid AND p.code='08S18';
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult'), modifieddate = now()
+FROM products p WHERE p.id = program_products.productid AND p.code='08S18Y';
 
-UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult')
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult'), modifieddate = now()
 FROM products p WHERE p.id = program_products.productid AND p.code='08S40';
 
-UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult')
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult'), modifieddate = now()
 FROM products p WHERE p.id = program_products.productid AND p.code='08S36';
 
-UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult')
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult'), modifieddate = now()
 FROM products p WHERE p.id = program_products.productid AND p.code='08S32';
 
-UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult')
-FROM products p WHERE p.id = program_products.productid AND p.code='08S18';
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult'), modifieddate = now()
+FROM products p WHERE p.id = program_products.productid AND p.code='08S18Z';
 
-UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult')
-FROM products p WHERE p.id = program_products.productid AND p.code='08S39';
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult'), modifieddate = now()
+FROM products p WHERE p.id = program_products.productid AND p.code='08S39Z';
 
-  UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult')
+  UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult'), modifieddate = now()
   FROM products p WHERE p.id = program_products.productid AND p.code='08S21';
 
-UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult')
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult'), modifieddate = now()
 FROM products p WHERE p.id = program_products.productid AND p.code='08S01';
 
-UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult')
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult'), modifieddate = now()
 FROM products p WHERE p.id = program_products.productid AND p.code='08S22';
 
-UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult')
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult'), modifieddate = now()
 FROM products p WHERE p.id = program_products.productid AND p.code='08S13';
 
-UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult')
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Adult'), modifieddate = now()
 FROM products p WHERE p.id = program_products.productid AND p.code='08S15';
 
-UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children')
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children'), modifieddate = now()
 FROM products p WHERE p.id = program_products.productid AND p.code='08S34B';
 
-UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children')
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children'), modifieddate = now()
 FROM products p WHERE p.id = program_products.productid AND p.code='08S32Z';
 
-UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children')
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children'), modifieddate = now()
 FROM products p WHERE p.id = program_products.productid AND p.code='08S42B';
 
-UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children')
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children'), modifieddate = now()
 FROM products p WHERE p.id = program_products.productid AND p.code='08S40Z';
 
-UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children')
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children'), modifieddate = now()
 FROM products p WHERE p.id = program_products.productid AND p.code='08S39B';
 
-UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children')
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children'), modifieddate = now()
 FROM products p WHERE p.id = program_products.productid AND p.code='08S39Y';
 
-UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children')
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children'), modifieddate = now()
 FROM products p WHERE p.id = program_products.productid AND p.code='08S01ZZ';
 
-UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children')
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children'), modifieddate = now()
 FROM products p WHERE p.id = program_products.productid AND p.code='08S20';
 
-UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children')
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children'), modifieddate = now()
 FROM products p WHERE p.id = program_products.productid AND p.code='08S19';
 
-UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children')
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Children'), modifieddate = now()
 FROM products p WHERE p.id = program_products.productid AND p.code='08S01B';
 
-UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Solution')
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Solution'), modifieddate = now()
 FROM products p WHERE p.id = program_products.productid AND p.code='08S23';
 
-UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Solution')
+UPDATE program_products SET productcategoryid = (SELECT id FROM product_categories WHERE name='Solution'), modifieddate = now()
 FROM products p WHERE p.id = program_products.productid AND p.code='08S17';
