d0dfa45677549bbdb646ba0da2a7e053b30bf6a3
Danni Yu <dyu@thoughtworks.com>
2016-10-28 17:26:12 +0800

    DY - #925 - change materialized view

--- deployment/create-materialized-view/migration/V5__lot_daily_full_soh_mv.sql
+++ deployment/create-materialized-view/migration/V5__lot_daily_full_soh_mv.sql
@@ -1,7 +1,8 @@
-DROP MATERIALIZED VIEW IF EXISTS vw_lot_daily_full_soh;
+DROP MATERIALIZED VIEW vw_lot_daily_full_soh;
 
 CREATE MATERIALIZED VIEW vw_lot_daily_full_soh AS
   (SELECT DISTINCT ON (facility_code, drug_code, occurred)
+     stock_card_entries.id                                                    AS stock_card_entry_id,
      facilities.name                                                          AS facility_name,
      facilities.code                                                          AS facility_code,
 
@@ -15,7 +16,7 @@ CREATE MATERIALIZED VIEW vw_lot_daily_full_soh AS
      products.primaryname                                                     AS drug_name,
 
      set_value(stock_card_entries.id, 'soh')                                  AS soh,
-     min(vw_lot_expiry_dates.expiration_date)                                 AS soonest_expiry_date,
+     MIN(vw_lot_expiry_dates.expiration_date)                                 AS soonest_expiry_date,
 
      stock_card_entries.occurred,
      stock_cards.modifieddate                                                 AS last_sync_date,
@@ -30,10 +31,10 @@ CREATE MATERIALIZED VIEW vw_lot_daily_full_soh AS
      JOIN geographic_zones AS parent_zone ON ZONE.parentid = parent_zone.id
      LEFT JOIN cmm_entries ON facilities.id = cmm_entries.facilityid
      LEFT JOIN vw_lot_expiry_dates ON vw_lot_expiry_dates.stock_card_entry_id = stock_card_entries.id
-                                      AND vw_lot_expiry_dates.lot_on_hand > 0
-   GROUP BY facilities.name, facilities.code, ZONE.name, ZONE.code,
-     parent_zone.name, parent_zone.code, products.primaryname, products.code,
-     facilities.id, stock_cards.modifieddate, stock_card_entries.id
+      AND vw_lot_expiry_dates.lot_on_hand > 0
+     GROUP BY facilities.name, facilities.code, ZONE.name, ZONE.code,
+      parent_zone.name, parent_zone.code, products.primaryname, products.code,
+      facilities.id, stock_cards.modifieddate, stock_card_entries.id
    ORDER BY facility_code, drug_code, occurred, stock_card_entries.createddate DESC);
 
 CREATE UNIQUE INDEX idx_vw_lot_daily_full_soh ON vw_lot_daily_full_soh (uuid, facility_code, drug_code, last_sync_date);
