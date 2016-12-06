bb70c970464ff1fe0039e9e640fee59ea1614e2a
Yi Deng <dydyistc@gmail.com>
2016-12-06 13:04:35 +0800

    YD/DY - #1007 - fix table name

--- deployment/db-migration/migration/V16__add_rapid_test_program_and_template.sql
+++ deployment/db-migration/migration/V16__add_rapid_test_program_and_template.sql
@@ -1,26 +1,26 @@
 INSERT INTO supplemental_programs (code, name, description, active)
 VALUES ('RAPID_TEST', 'Rapid Test', 'Rapid test', TRUE);
 
-INSERT INTO program_data_columns (code, supplmentalProgramId) VALUES
+INSERT INTO program_data_columns (code, supplementalProgramId) VALUES
 ('HIV-DETERMINE-CONSUME', (SELECT id FROM supplemental_programs WHERE code = 'RAPID_TEST'));
 
-INSERT INTO program_data_columns (code, supplmentalProgramId) VALUES
+INSERT INTO program_data_columns (code, supplementalProgramId) VALUES
 ('HIV-DETERMINE-POSITIVE', (SELECT id FROM supplemental_programs WHERE code = 'RAPID_TEST'));
 
-INSERT INTO program_data_columns (code, supplmentalProgramId) VALUES
+INSERT INTO program_data_columns (code, supplementalProgramId) VALUES
 ('HIV-UNIGOLD-CONSUME', (SELECT id FROM supplemental_programs WHERE code = 'RAPID_TEST'));
 
-INSERT INTO program_data_columns (code, supplmentalProgramId) VALUES
+INSERT INTO program_data_columns (code, supplementalProgramId) VALUES
 ('HIV-UNIGOLD-POSITIVE', (SELECT id FROM supplemental_programs WHERE code = 'RAPID_TEST'));
 
-INSERT INTO program_data_columns (code, supplmentalProgramId) VALUES
+INSERT INTO program_data_columns (code, supplementalProgramId) VALUES
 ('SYPHILLIS-CONSUME', (SELECT id FROM supplemental_programs WHERE code = 'RAPID_TEST'));
 
-INSERT INTO program_data_columns (code, supplmentalProgramId) VALUES
+INSERT INTO program_data_columns (code, supplementalProgramId) VALUES
 ('SYPHILLIS-POSITIVE', (SELECT id FROM supplemental_programs WHERE code = 'RAPID_TEST'));
 
-INSERT INTO program_data_columns (code, supplmentalProgramId) VALUES
+INSERT INTO program_data_columns (code, supplementalProgramId) VALUES
 ('MALARIA-CONSUME', (SELECT id FROM supplemental_programs WHERE code = 'RAPID_TEST'));
 
-INSERT INTO program_data_columns (code, supplmentalProgramId) VALUES
+INSERT INTO program_data_columns (code, supplementalProgramId) VALUES
 ('MALARIA-POSITIVE', (SELECT id FROM supplemental_programs WHERE code = 'RAPID_TEST'));
