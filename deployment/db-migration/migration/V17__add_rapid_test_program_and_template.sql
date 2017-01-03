090bf50dfb42a373705ae71e3cfa5bbd4a12704e
Danni Yu <dyu@thoughtworks.com>
2017-01-03 18:22:56 +0800

    DY - #000 - change rapid test migration to be more tolerant

--- deployment/db-migration/migration/V17__add_rapid_test_program_and_template.sql
+++ deployment/db-migration/migration/V17__add_rapid_test_program_and_template.sql
@@ -1,26 +1,62 @@
 INSERT INTO supplemental_programs (code, name, description, active)
-VALUES ('RAPID_TEST', 'Rapid Test', 'Rapid test', TRUE);
+SELECT 'RAPID_TEST', 'Rapid Test', 'Rapid test', TRUE
+WHERE
+  NOT EXISTS (
+    SELECT code FROM supplemental_programs WHERE code = 'RAPID_TEST'
+  );
 
-INSERT INTO program_data_columns (code, supplementalProgramId) VALUES
-('CONSUME_HIVDETERMINE', (SELECT id FROM supplemental_programs WHERE code = 'RAPID_TEST'));
+INSERT INTO program_data_columns (code, supplementalProgramId) SELECT
+'CONSUME_HIVDETERMINE', (SELECT id FROM supplemental_programs WHERE code = 'RAPID_TEST')
+WHERE
+  NOT EXISTS (
+    SELECT code FROM program_data_columns WHERE code = 'CONSUME_HIVDETERMINE'
+  );
 
-INSERT INTO program_data_columns (code, supplementalProgramId) VALUES
-('POSITIVE_HIVDETERMINE', (SELECT id FROM supplemental_programs WHERE code = 'RAPID_TEST'));
+INSERT INTO program_data_columns (code, supplementalProgramId) SELECT
+'POSITIVE_HIVDETERMINE', (SELECT id FROM supplemental_programs WHERE code = 'RAPID_TEST')
+WHERE
+  NOT EXISTS (
+    SELECT code FROM program_data_columns WHERE code = 'POSITIVE_HIVDETERMINE'
+  );
 
-INSERT INTO program_data_columns (code, supplementalProgramId) VALUES
-('CONSUME_HIVUNIGOLD', (SELECT id FROM supplemental_programs WHERE code = 'RAPID_TEST'));
+INSERT INTO program_data_columns (code, supplementalProgramId) SELECT
+'CONSUME_HIVUNIGOLD', (SELECT id FROM supplemental_programs WHERE code = 'RAPID_TEST')
+WHERE
+  NOT EXISTS (
+    SELECT code FROM program_data_columns WHERE code = 'CONSUME_HIVUNIGOLD'
+  );
 
-INSERT INTO program_data_columns (code, supplementalProgramId) VALUES
-('POSITIVE_HIVUNIGOLD', (SELECT id FROM supplemental_programs WHERE code = 'RAPID_TEST'));
+INSERT INTO program_data_columns (code, supplementalProgramId) SELECT
+'POSITIVE_HIVUNIGOLD', (SELECT id FROM supplemental_programs WHERE code = 'RAPID_TEST')
+WHERE
+  NOT EXISTS (
+    SELECT code FROM program_data_columns WHERE code = 'POSITIVE_HIVUNIGOLD'
+  );
 
-INSERT INTO program_data_columns (code, supplementalProgramId) VALUES
-('CONSUME_SYPHILLIS', (SELECT id FROM supplemental_programs WHERE code = 'RAPID_TEST'));
+INSERT INTO program_data_columns (code, supplementalProgramId) SELECT
+'CONSUME_SYPHILLIS', (SELECT id FROM supplemental_programs WHERE code = 'RAPID_TEST')
+WHERE
+  NOT EXISTS (
+    SELECT code FROM program_data_columns WHERE code = 'CONSUME_SYPHILLIS'
+  );
 
-INSERT INTO program_data_columns (code, supplementalProgramId) VALUES
-('POSITIVE_SYPHILLIS', (SELECT id FROM supplemental_programs WHERE code = 'RAPID_TEST'));
+INSERT INTO program_data_columns (code, supplementalProgramId) SELECT
+'POSITIVE_SYPHILLIS', (SELECT id FROM supplemental_programs WHERE code = 'RAPID_TEST')
+WHERE
+  NOT EXISTS (
+    SELECT code FROM program_data_columns WHERE code = 'POSITIVE_SYPHILLIS'
+  );
 
-INSERT INTO program_data_columns (code, supplementalProgramId) VALUES
-('CONSUME_MALARIA', (SELECT id FROM supplemental_programs WHERE code = 'RAPID_TEST'));
+INSERT INTO program_data_columns (code, supplementalProgramId) SELECT
+'CONSUME_MALARIA', (SELECT id FROM supplemental_programs WHERE code = 'RAPID_TEST')
+WHERE
+  NOT EXISTS (
+    SELECT code FROM program_data_columns WHERE code = 'CONSUME_MALARIA'
+  );
 
-INSERT INTO program_data_columns (code, supplementalProgramId) VALUES
-('POSITIVE_MALARIA', (SELECT id FROM supplemental_programs WHERE code = 'RAPID_TEST'));
+INSERT INTO program_data_columns (code, supplementalProgramId) SELECT
+'POSITIVE_MALARIA', (SELECT id FROM supplemental_programs WHERE code = 'RAPID_TEST')
+WHERE
+  NOT EXISTS (
+    SELECT code FROM program_data_columns WHERE code = 'POSITIVE_MALARIA'
+  );
